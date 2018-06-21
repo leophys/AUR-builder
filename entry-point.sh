38d2e1341669fbb66d6c7217cd6714da14422f96
Blallo <blallo@autistici.org>
2018-06-21 12:51:13 +0200

    Recursively fetch missing (old) packages

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 PKG=$(echo $1|awk -F":" '{print $1}')
 INSTALL=$(echo $1|awk -F":" '{print $2}')
 
@@ -17,7 +19,21 @@ else
     cd $PKG
 fi
 
-makepkg -s
+function install_wrapper {
+    err="/tmp/makepkg-$RANDOM"
+    makepkg -s --noconfirm 2>&1| tee -a $err
+    failed_pkgs=$(cat $err|grep "error: failed"|awk -F\' '{print $2}')
+    if [[ $failed_pkgs != "" ]]; then
+        for pkg in $failed_pkgs; do
+            pkg_name=$(echo $pkg|awk 'match($0, /[a-zA-Z0-9-]+/) {print substr($0, RSTART, RLENGTH-2)}'|sed -e 's/^\([a-zA-Z0-9]\+\)-\?$/\1/')
+            pkg_url="https://archive.archlinux.org/packages/$(echo $pkg_name|cut -c 1)/$pkg_name/$pkg"
+            sudo pacman -U $pkg_url --noconfirm
+        done
+        install_wrapper
+    fi
+}
+
+install_wrapper
 
 if test $INSTALL; then
     sudo pacman -U *.pkg.tar.xz
